5bfa85989770e21b46e4047aa5aa85499f8928fc
Libby Horacek <libby@daydrea.me>
2017-12-26 18:10:43 -0500

    Add a navigation bar

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
     <header>
       <h1> Math Garden </h1>
+
+      <nav>
+        <ul>
+          <li><router-link :to="{ name: 'SignUp' }">Sign up</router-link></li>
+          <li><router-link :to="{ name: 'Learners' }">Learners</router-link></li>
+        </ul>
+      </nav>
     </header>
 
     <main>
@@ -24,7 +31,8 @@ header { padding: 16px;
          padding-left: 24px }
 
 main { display: flex;
-       flex-direction: column }
+       flex-direction: column;
+       margin-top: 36px }
 
 .fade-enter-active, .fade-leave-active {
     transition: opacity .5s
@@ -32,4 +40,13 @@ main { display: flex;
 .fade-enter, .fade-leave-to {
     opacity: 0
 }
+
+header { display: flex;
+         flex-direction: row;
+         border: 2px solid black;
+         border-width: 0 0 2px 0; }
+
+nav { align-self: center }
+
+nav > ul > li { display: inline-block; margin-left: 16px }
 </style>

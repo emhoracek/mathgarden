3fc4c1792ae792fd06bc0070ceb41935c49f974b
Libby Horacek <libby@daydrea.me>
2017-12-26 20:22:07 -0500

    Improve usability of sign up form
    Make it clear which fields are required, and explain wierd fields.

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -4,25 +4,32 @@
     <form @submit="submit">
       <ul>
         <li>
-          <label for="email">Email: </label>
+          <label for="email">Email <span class="required">(required)</span></label>
           <input name="emai" v-model="learner.email" />
-          <p class="email-errors" v-if="errors.email">
+          <p class="error full-width" v-if="errors.email">
             {{ errors.email }}
           </p>
         </li>
         <li>
-          <label>Name: </label>
+          <label>Name <span class="required">(optional)</span></label>
           <input v-model="learner.name" />
         </li>
         <li>
-          <label>LG Slack name: </label>
+          <label>LG Slack user <span class="required">(optional)</span></label>
           <input v-model="learner.slack_name" />
+          <p class="full-width">If you are a <a href="https://lg-slack-automate.herokuapp.com/">Learning Gardens Slack chat</a> user, enter your username here.</p>
         </li>
         <li>
-          <label>Goal: </label>
+          <label>Goal <span class="required">(optional)</span></label>
           <textarea v-model="learner.goal" />
+          <p class="full-width">Share what you would like to achieve by joining Learning Gardens.</p>
         </li>
       </ul>
+
+      <p class="error errors" v-if="errors.general">
+        {{ errors.general }}
+      </p>
+
       <p class="button">
         <button>Create new account</button>
       </p>
@@ -41,7 +48,8 @@
           goal: ''
         },
         errors: {
-          email: ''
+          email: '',
+          general: ''
         }
       }
     },
@@ -83,6 +91,13 @@
             body: JSON.stringify(this.learner)
           }).then((resp) => {
             return resp.json()
+          }).then((json) => {
+            this.errors = {}
+            if (json.id) {
+              this.$router.push({ name: 'Learner', params: { 'id': json.id } })
+            } else {
+              this.errors.general = json.error || 'Please try again.'
+            }
           })
       }
     }
@@ -94,10 +109,9 @@ div.wrapper { width: 90%;
               max-width: 550px;
               margin: 0 auto }
 
-.email-errors { width: 100%;
-                margin: 4px;
-                color: red;
-                text-align: center }
+.error { color: red; }
+.full-width { width: 100% }
+.required { color: grey }
 
 form { margin: auto }
 
@@ -106,9 +120,11 @@ ul { padding-left: 0 }
 ul li { display: flex;
         flex-wrap: wrap;
         align-items: center;
-        margin: 8px }
+        margin: 16px 0 16px 0; }
 
-li p { display: block }
+li p { display: block;
+       margin: 8px 0 8px 0;
+       color: darkgrey }
 
 label { flex: 1 0 120px;
         max-width: 220px;
@@ -132,6 +148,7 @@ button { text-align: center;
          align-self: end;
          font-size: 16px;
          padding: 8px;
-         margin-right: 8px }
-
+         margin-right: 8px;
+         border: 1px solid black;
+         border-radius: 5px; }
 </style>
